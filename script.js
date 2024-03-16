f2ca328403a948cc0f92e2da8c2d44287d61f261
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-16 19:24:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let ColorSelect = document.getElementById('colorSelect');
+let SelectRemove = document.getElementById('btn');
+SelectRemove.addEventListener("click",()=>{
+  ColorSelect.options[ColorSelect.selectedIndex].remove();
+})
